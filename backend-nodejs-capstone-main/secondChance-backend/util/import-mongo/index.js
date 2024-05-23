e7f906917650181127780b21bc9e214159b79737
shayan esfahanizadeh <106907105+Shayan-esf@users.noreply.github.com>
2024-05-23 12:36:08 +0330

    good
    +_+

---
+++ backend-nodejs-capstone-main/secondChance-backend/util/import-mongo/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient
+const fs = require('fs')
+const path = require('path')
+
+const url = `${process.env.MONGO_URL}`
+const filename = path.join(__dirname, 'secondChanceItems.json')
+const dbName = 'secondChance'
+const collectionName = 'secondChanceItems'
+const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs
+
+async function loadData () {
+  const client = new MongoClient(url)
+  try {
+    await client.connect()
+    console.log('Connected successfully to server')
+    const db = client.db(dbName)
+    const collection = db.collection(collectionName)
+    const cursor = await collection.find({})
+    const documents = await cursor.toArray()
+    if (documents.length === 0) {
+      const insertResult = await collection.insertMany(data)
+      console.log('Inserted documents:', insertResult.insertedCount)
+    } else {
+      console.log('Items already exists in DB')
+    }
+  } catch (err) {
+    console.error(err)
+  } finally {
+    await client.close()
+  }
+}
+
+loadData()
+
+module.exports = {
+  loadData
+}
